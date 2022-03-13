c0d36c4d75cfbdfca58b2d87e988eccc4e240b3a
Andrii Kryvoruk <AndriiK@neogames.com>
2022-03-13 17:22:49 +0200

    sfc components application done

---
+++ projects/sfc-components-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sfc-components-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sfc-components-app';
+}
