929a1216c0786731faf4c7a826b3225d1910645e
Andrii Kryvoruk <AndriiK@neogames.com>
2022-03-16 21:56:44 +0200

    add lint3

--- projects/ngx-sfc-common/src/lib/components/button/button.component.ts
+++ projects/ngx-sfc-common/src/lib/components/button/button.component.ts
@@ -15,7 +15,7 @@ export class ButtonComponent {
   @Input()
   text: string = this.BUTTON_DEFAULT_TEXT;
 
-  @Input('icon-defore')
+  @Input('icon-ddefore')
   iconBefore: string | null = null;
 
   @Input()
