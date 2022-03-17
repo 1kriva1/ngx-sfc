4fe9fd35f070fab6f5febf67e45deb4dc8c65d40
Andrii Kryvoruk <AndriiK@neogames.com>
2022-03-17 10:40:56 +0200

    add code coverage done

--- projects/ngx-sfc-inputs/karma.conf.js
+++ projects/ngx-sfc-inputs/karma.conf.js
@@ -30,7 +30,16 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        emitWarning: false,
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
